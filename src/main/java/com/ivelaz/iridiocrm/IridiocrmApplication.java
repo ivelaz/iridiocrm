65002ff42e14743f9ce06b10567b54f01280f04d
ivelaz <i.velaz@hotmail.com>
2017-08-17 23:32:42 +0200

    Versión cero del programa.

---
+++ src/main/java/com/ivelaz/iridiocrm/IridiocrmApplication.java
@@ -0,0 +1,12 @@
+package com.ivelaz.iridiocrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IridiocrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IridiocrmApplication.class, args);
+	}
+}
