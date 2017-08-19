176731b6560f410e84d4bdd33594a17814f762db
ivelaz <i.velaz@hotmail.com>
2017-08-19 02:30:58 +0200

    Versión: 19_08_17_01

---
+++ src/main/java/com/ivelaz/iridiocrm/constants/ConstantesVistas.java
@@ -0,0 +1,12 @@
+package com.ivelaz.iridiocrm.constants;
+
+
+/**
+ * Constantes relacionadas con las vistas.  * 
+ */
+public class ConstantesVistas {
+	
+	/** The Constant CLIENTES_FORM. */
+	public static final String CLIENTES_FORM = "clientesform";
+
+}
--- src/main/java/com/ivelaz/iridiocrm/controllers/ClientesController.java
+++ src/main/java/com/ivelaz/iridiocrm/controllers/ClientesController.java
@@ -1,16 +1,54 @@
 package com.ivelaz.iridiocrm.controllers;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ivelaz.iridiocrm.constants.ConstantesVistas;
+import com.ivelaz.iridiocrm.models.ClienteModel;
+
+
+
+/**
+ * Mapea las URLs de operaciones relacionadas con
+ * clientes.
+ */
 @Controller
+@RequestMapping("/clientes")
 public class ClientesController {
 	
+	private static final Log LOG = LogFactory.getLog(ClientesController.class);
+	
+	/**
+	 * Mostrar formulario.
+	 * URL: localhost:8080/clientes/crearcliente
+	 * @return un ModelAndView con la vista y un 
+	 * ClienteModel vacío para utilizarlo en el formulario.
+	 */
 	@GetMapping("/crearcliente")
 	public ModelAndView mostrarFormulario() {
-		ModelAndView mav = new ModelAndView("clientesform");
+		ModelAndView mav = new ModelAndView(ConstantesVistas.CLIENTES_FORM);
+		ClienteModel cliente = new ClienteModel();
+		mav.addObject("cliente", cliente);
 		return mav;
+	}
+	
+	/**
+	 * Procesa el formulario crear Cliente.
+	 * URL: localhost:8080/clientes/addCliente
+	 * @param cliente el objeto devuelto relleno por el formulario.
+	 * @return la vista.
+	 */
+	@PostMapping("/addCliente")
+	public String addCliente(@ModelAttribute(name="cliente") ClienteModel cliente) {
+		LOG.info("Método: addCliente() -- Parámetro: " + cliente.toString());		
+		return "redirect:/clientes/crearcliente";
+		
 	}
 	
 }
