1e1b117c165e4d91115a313caa4a95bc1547d234
ivelaz <i.velaz@hotmail.com>
2017-08-20 06:46:45 +0200

    Versión: 20_08_17_02 ClienteService y ClienteServiceImpl

---
+++ src/main/java/com/ivelaz/iridiocrm/services/impl/ClienteServiceImpl.java
@@ -0,0 +1,80 @@
+package com.ivelaz.iridiocrm.services.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ivelaz.iridiocrm.components.ClienteConverter;
+import com.ivelaz.iridiocrm.entities.Cliente;
+import com.ivelaz.iridiocrm.models.ClienteModel;
+import com.ivelaz.iridiocrm.repositories.ClienteRepository;
+import com.ivelaz.iridiocrm.services.ClienteService;
+
+@Service("clienteServiceImpl")
+public class ClienteServiceImpl implements ClienteService{
+
+	@Autowired
+	@Qualifier("clienteRepository")
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	@Qualifier("clienteConverter")
+	private ClienteConverter clienteConverter;
+	
+	
+	@Transactional(readOnly=true)
+	@Override
+	public ClienteModel addCliente(ClienteModel clienteModel) {
+		Cliente cliente = clienteRepository.save(clienteConverter.ClienteModelACliente(clienteModel));
+		return clienteConverter.ClienteAClienteModel(cliente);
+	}
+	
+	@Transactional(readOnly=true)
+	@Override
+	public List<ClienteModel> listarClientes() {
+		List<ClienteModel> clienteModels = new ArrayList<ClienteModel>();
+		List<Cliente> clientes = clienteRepository.findAll();
+		for(Cliente cliente : clientes) {
+			clienteModels.add(clienteConverter.ClienteAClienteModel(cliente));
+		}
+		return clienteModels;
+	}
+
+	@Transactional(readOnly=true)
+	@Override
+	public List<ClienteModel> buscarPorNombre(String palabraClave) {
+		List<ClienteModel> clienteModels = new ArrayList<ClienteModel>();
+		List<Cliente> clientes =clienteRepository.buscarPorNombre(palabraClave);
+		for(Cliente cliente : clientes) {
+			clienteModels.add(clienteConverter.ClienteAClienteModel(cliente));
+		}
+		return clienteModels;
+	}
+
+	@Transactional(readOnly=true)
+	@Override
+	public Cliente buscarClientePorId(int id) {
+		return clienteRepository.findById(id);
+	}
+
+	@Transactional(readOnly=true)
+	@Override
+	public ClienteModel buscarClienteModelPorId(int id) {
+		return clienteConverter.ClienteAClienteModel(buscarClientePorId(id));
+	}
+
+	@Transactional
+	@Override
+	public void borrarCliente(int id) {
+		Cliente cliente = buscarClientePorId(id);
+		if (cliente != null) {
+			clienteRepository.delete(cliente);
+		}
+	}
+	
+
+}
