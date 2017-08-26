ffbbe37a099f02ee30a1eeda69441ac4a4af19da
ivelaz <i.velaz@hotmail.com>
2017-08-26 23:01:26 +0200

    Versión: 26_08_17_05 previo plantillas 404 y 500

--- src/main/java/com/ivelaz/iridiocrm/constants/ConstantesVistas.java
+++ src/main/java/com/ivelaz/iridiocrm/constants/ConstantesVistas.java
@@ -4,6 +4,8 @@ package com.ivelaz.iridiocrm.constants;
 
 public class ConstantesVistas {	
 	
+	public static final String INICIO = "index";
+	
 	public static final String CLIENTES_FORM = "clientesform";	
 
 	public static final String CLIENTES_LIST = "listaclientes";
